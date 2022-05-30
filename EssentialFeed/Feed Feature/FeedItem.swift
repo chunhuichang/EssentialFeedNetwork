9f407edc5226650a124207032108ca86c3d8d8ba
Chun Hui Chang <jill20021@gmail.com>
2022-05-30 15:26:55 +0800

    docs: start create RemoteFeedLoader from TDD

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jill Chang on 2022/5/30.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
