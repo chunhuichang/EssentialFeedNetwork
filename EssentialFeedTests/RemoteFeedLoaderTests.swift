6f0f30b95d71581fc72f9c1483929837b334028b
Chun Hui Chang <jill20021@gmail.com>
2022-05-31 10:39:49 +0800

    test: Move test logic to a separate type `HTTPClientSpy`

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,27 +9,42 @@ import XCTest
 
 class RemoteFeedLoader {
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        // step2.move the test logic from the RemoteFeedLoader to HTTPClient
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
 class HTTPClient {
-    static let shared = HTTPClient()
-    
-    private init() {}
+    // step1.make the shared instance a variable
+    static var shared = HTTPClient()
+    // step5. remove HTTPClient private initializer since it's not a Singleton anymore.
+        
+    func get(from url: URL) {}
+}
+
+
+class HTTPClientSpy: HTTPClient {
     
     var requestedURL: URL?
+    
+    override func get(from url: URL) {
+        // step3.move the test logic to a new subclass of theHTTPClient
+        requestedURL = url
+    }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init() throws {
-        let client = HTTPClient.shared
+        // step4.swap the HTTPClient shared instance with the spy subclass during test.
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteFeedLoader()
         XCTAssertNil(client.requestedURL)
     }
     
     func test_requestDataFromURL() throws {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteFeedLoader()
         sut.load()
         XCTAssertNotNil(client.requestedURL)
