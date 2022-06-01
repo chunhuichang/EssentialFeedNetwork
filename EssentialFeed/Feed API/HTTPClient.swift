1af83d7dd09a7d0369d7c868f85d06632fee4c98
Chun Hui Chang <jill20021@gmail.com>
2022-06-01 15:50:37 +0800

    docs: Move HTTPClient to its own file

---
+++ EssentialFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Jill Chang on 2022/6/1.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
