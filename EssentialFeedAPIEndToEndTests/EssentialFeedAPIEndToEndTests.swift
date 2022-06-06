e53bc9a73481e8ee2e122515987c77d18ab35dda
Chun Hui Chang <jill20021@gmail.com>
2022-06-06 21:40:08 +0800

    build: Add end-to-end API tests in test target

---
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Jill Chang on 2022/6/6.
+//
+
+import XCTest
+
+class EssentialFeedAPIEndToEndTests: XCTestCase {
+    func testExample() {
+    }
+}
